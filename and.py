5acf8b2479626d821ad6db800172f08cf8890a72
adriano.adodo <adriano.adodo@dataprev.gov.br>
2017-11-23 16:32:37 -0300

    Rede neural AND

---
+++ and.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 14:19:01 2017
+
+@author: dodo
+"""
+
+entradas=[[0, 0],[0, 1], [1, 0], [1, 1]]
+pesos=[0, 0]
+saidaEsperada=[0, 0, 0, 1]
+
+saida=[]
+
+def somatorio():    
+    for i in range(entradas[:]):
+        soma = 0
+        soma = entradas[i][0]*pesos[0]+entradas[i][1]*pesos[1]
+        
+def novosPesos():
+    
