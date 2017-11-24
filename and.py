6b29cde83674d7c488f1ef47caebefd9c7dd52b6
Adriano <adrianoalvesdodo@gmail.com>
2017-11-24 01:39:14 -0300

    implementação da rede AND

--- and.py
+++ and.py
@@ -7,15 +7,41 @@ Created on Thu Nov 23 14:19:01 2017
 """
 
 entradas=[[0, 0],[0, 1], [1, 0], [1, 1]]
-pesos=[0, 0]
+pesos=[0.0, 0.0]
 saidaEsperada=[0, 0, 0, 1]
+txAprendizagem=0.1
 
 saida=[]
 
+def pesosFinais():
+    calculaPeso=somatorio()
+    if (calculaPeso>0):
+        calculaPeso=somatorio()
+    
+
 def somatorio():    
-    for i in range(entradas[:]):
+    erro=0
+    for i in range(len(entradas)):
         soma = 0
         soma = entradas[i][0]*pesos[0]+entradas[i][1]*pesos[1]
-        
-def novosPesos():
+        stp=stepFunction(soma, saidaEsperada[i])
+        if(stp != 1):
+            erro= saidaEsperada[i] - soma
+            peso1 = atualizaPesos(pesos[0],entradas[i][0],erro)
+            peso2 = atualizaPesos(pesos[1],entradas[i][0],erro)
+            pesos[:] = [peso1,peso2]            
+            erro+=1
+    return erro
     
+def atualizaPesos(pesoAnterior, entrada, erro):
+    # erro=respostaCorreta-respostaCalculada
+    novoPeso = pesoAnterior+(txAprendizagem*entrada*erro)
+    return novoPeso
+
+
+def stepFunction(valor, valorEsperado):
+    if (valor == valorEsperado):
+        return 1
+    return 0
+
+pesosFinais()
